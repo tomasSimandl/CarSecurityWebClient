b496b1638fba12dad12735ed7a63586b71d91d3b
Tomáš Šimandl <tomas.simandl@gmail.com>
2019-02-22 14:19:57 +0100

    fix error handler

--- src/main/kotlin/com/carsecurity/web/config/CustomErrorAttributes.kt
+++ src/main/kotlin/com/carsecurity/web/config/CustomErrorAttributes.kt
@@ -7,7 +7,6 @@ import org.springframework.boot.web.servlet.error.DefaultErrorAttributes
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.client.HttpClientErrorException
-import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.ModelAndView
 import javax.servlet.http.HttpServletRequest
@@ -23,7 +22,7 @@ class CustomErrorAttributes: DefaultErrorAttributes() {
 
     override fun resolveException(request: HttpServletRequest, response: HttpServletResponse, handler: Any?, ex: Exception): ModelAndView? {
 
-        var message = "Can not get data from server"
+        var message = "Can not communicate with server"
         val statusCode: Int
 
         if(ex is HttpClientErrorException) {
@@ -37,9 +36,9 @@ class CustomErrorAttributes: DefaultErrorAttributes() {
             } else {
                 statusCode = ex.statusCode.value()
                 try {
-                    val json = JsonParser().parse(ex.message) as JsonObject
+                    val json = JsonParser().parse(ex.responseBodyAsString) as JsonObject
                     if(json.has("error")){
-                        message = json["error"].toString()
+                        message = json["error"].toString().trim('"')
                     }
                 }catch (e: Exception){
                     logger.debug("Response can not be parsed to JSON.")
@@ -48,7 +47,7 @@ class CustomErrorAttributes: DefaultErrorAttributes() {
         } else {
             logger.error("Controller ends with exception:", ex)
             statusCode = HttpServletResponse.SC_BAD_REQUEST
-            message = "Can not get data from server: ${ex.message}"
+            message = "Can not communicate with server: ${ex.message}"
         }
 
         request.setAttribute(errorStatusCode, statusCode)
@@ -59,11 +58,26 @@ class CustomErrorAttributes: DefaultErrorAttributes() {
 
     override fun getErrorAttributes(webRequest: WebRequest, includeStackTrace: Boolean): MutableMap<String, Any> {
 
+        // Get data from resolveException method
         var message = webRequest.getAttribute(errorMessage, 0) as String?
         var statusCode = webRequest.getAttribute(errorStatusCode, 0) as Int?
 
-        if(message == null || message.isBlank()) message = "Unknown error"
-        if(statusCode == null || statusCode == 0) statusCode = 500
+        if(statusCode == null || statusCode == 0) {
+            // Get data given by Spring
+            statusCode = webRequest.getAttribute("javax.servlet.error.status_code", 0) as Int?
+        }
+
+        if(message == null || message.isBlank()) {
+            message = if(statusCode != null) {
+                HttpStatus.resolve(statusCode).toString()
+            } else {
+                "Unknown error!"
+            }
+        }
+
+        if(statusCode == null || statusCode == 0){
+            statusCode = HttpServletResponse.SC_INTERNAL_SERVER_ERROR
+        }
 
         webRequest.setAttribute("javax.servlet.error.status_code", statusCode, 0)
 
