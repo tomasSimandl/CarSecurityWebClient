8fc69b627cb9533f5d4be8445e72df3d0b793cfa
Tomáš Šimandl <tomas.simandl@gmail.com>
2019-02-22 19:03:39 +0100

    disable csrf

--- src/main/kotlin/com/carsecurity/web/config/SecurityConfig.kt
+++ src/main/kotlin/com/carsecurity/web/config/SecurityConfig.kt
@@ -28,6 +28,8 @@ class SecurityConfig(
                 .and()
                 .formLogin()
                 .defaultSuccessUrl("/test") // TODO
+                .and()
+                .csrf().disable()
 
     }
 }
