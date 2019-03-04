9a835f2ea2630dc7e1e6b1d2e239c45b6a5b0a3f
Tomáš Šimandl <tomas.simandl@gmail.com>
2019-03-04 21:25:51 +0100

    Model which missing in last commit

---
+++ src/main/kotlin/com/carsecurity/web/rest/model/Status.kt
@@ -0,0 +1,21 @@
+package com.carsecurity.web.rest.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Status(
+
+        val battery: Float,
+
+        @JsonProperty("is_charging")
+        val isCharging: Boolean,
+
+        @JsonProperty("is_power_save_mode")
+        val powerSaveMode: Boolean,
+
+        val utils: Map<String, Boolean>,
+
+        val time: Long,
+
+        @JsonProperty("car_id")
+        val carId: Long
+)
